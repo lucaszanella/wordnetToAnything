35373504764ca2c837be1e13862a0321eb99fc49
Lucas Zanella <me@lucaszanella.com>
2017-02-14 03:21:06 -0200

    toMongo almost done

--- toMongo.py
+++ toMongo.py
@@ -2,10 +2,15 @@ import wordnetToAnything as WN
 from pymongo import MongoClient
 
 client = MongoClient()
-
+databaseName = 'wordnet'
 
 def toMongo(line, args):
-    print('must insert line ' + str(line) + ' to database ' + args[0])
+    print('must insert line ' + str(line) + ' to database ' + replacePointWithUnderscore(args[0]))
 
-client['index.verb'].drop_collection() #in case you have written something there before
-WN.forEachLineOfFileDo('index.verb', WN.CallbackWrapper(toMongo, 'index.verb'))
+def replacePointWithUnderscore(string):
+    return string.replace('.','_')
+
+#erases collection in case you have written something there before
+fileName = 'index.verb'
+client[databaseName].drop_collection(replacePointWithUnderscore(fileName))
+WN.forEachLineOfFileDo(fileName, WN.CallbackWrapper(toMongo, fileName))
