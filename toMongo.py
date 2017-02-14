5e0db9c7e6fac183f2c6eb1058f05c30dafd7252
Lucas Zanella <me@lucaszanella.com>
2017-02-14 03:16:07 -0200

    renames toMongo

--- toMongo.py
+++ toMongo.py
@@ -3,10 +3,9 @@ from pymongo import MongoClient
 
 client = MongoClient()
 
-#forEachLineOfFileDo('data.verb', print)
 
 def toMongo(line, args):
     print('must insert line ' + str(line) + ' to database ' + args[0])
 
-#db.drop_collection('index.verb')
+client['index.verb'].drop_collection() #in case you have written something there before
 WN.forEachLineOfFileDo('index.verb', WN.CallbackWrapper(toMongo, 'index.verb'))
