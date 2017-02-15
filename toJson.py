be7199f835e7ba126532a7794d4a1ea45ab22ec5
Lucas Zanella <me@lucaszanella.com>
2017-02-14 23:07:02 -0200

    toJson now respects pyLint specs

--- toJson.py
+++ toJson.py
@@ -7,40 +7,39 @@ Lucas Zanella, 13/02/2017
 import wordnetToAnything as WN
 import json
 
-def toJson(line, kwargs):
-    originalFileName = kwargs['originalFileName']
-    fileObject = kwargs['fileObject']
+def to_json(line, kwargs):
+    """Inserts a new line into kwargs['file_object'] file object"""
+    original_file_name = kwargs['original_file_name']
+    file_object = kwargs['file_object']
+    if 'index' in original_file_name: #if it's an index file
     #We pop the counters as we don't need them in json
-    if 'index' in originalFileName: #if it's an index file
         line.pop('p_cnt', None)
         line.pop('synset_cnt', None)
         line.pop('sense_cnt', None)
         line.pop('tagsense_cnt', None)
-    if 'data' in originalFileName and 'wn-' not in originalFileName: #if it's a data file but not a wn-lang-data... file
+    if 'data' in original_file_name and 'wn-' not in original_file_name:
+    #if it's a data file but not a wn-lang-data... file
         line.pop('w_cnt', None)
         line.pop('p_cnt', None)
         line.pop('sense_cnt', None)
         line.pop('tagsense_cnt', None)
     comma = ''
-    newLine = '\n'
+    new_line = '\n'
     #if not firstLine, change comma
-    fileObject.write(comma + newLine + json.dumps(line))
-
-def justPrint(line, args):
-    print(line)
+    file_object.write(comma + new_line + json.dumps(line))
 
 print('working...\n')
 #Takes care of index and data files
 
 #erases collection in case you have written something there before
-originalFileName = 'index.verb'
-newFileName = originalFileName + '.json'
-with open(newFileName, 'w') as f:
+original_file_name = 'index.verb'
+new_file_name = original_file_name + '.json'
+with open(new_file_name, 'w') as f:
     f.write('[')
-    WN.forEachLineOfFileDo(originalFileName, 
-        WN.CallbackWrapper(toJson, 
-            originalFileName = originalFileName, 
-            fileObject = f
+    WN.forEachLineOfFileDo(original_file_name, 
+        WN.CallbackWrapper(to_json, 
+            original_file_name = original_file_name, 
+            file_object = f
         )
     )
     f.write(']')
@@ -53,5 +52,5 @@ language = 'pt'
 fileName = 'wn-data-por.tab'
 collectionName = language + '_' + replacePointWithUnderscore(fileName)
 client[databaseName].drop_collection(collectionName)
-WN.forEachLineOfFileDo(fileName, WN.CallbackWrapper(toMongo, fileName, collectionName))
+WN.forEachLineOfFileDo(fileName, WN.CallbackWrapper(to_json, fileName, collectionName))
 '''
